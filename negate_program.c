74bb22386d9789135ef1e82fce4f06437a412422
Ivo Georgiev <ivogeorg@gmail.com>
2019-10-22 13:22:43 -0600

    Initial commit

---
+++ negate_program.c
@@ -0,0 +1,19 @@
+// global data
+static int value = 6;
+
+// A function that returns its
+// argument negated
+int negate(int a) {
+    return (-1) * a;
+}
+
+// Every program in the C language always starts in the 'main()' function
+int main() {
+    // Calling another function will
+    // cause a new stack frame to be
+    // set up for it
+    if (value > 0)
+        value = negate(value);
+
+    return 0;
+}
